b6f0a41e132857b6bc774e1ec2f284519f82f455
Roman <romanvasylyna888@gmail.com>
2021-06-16 17:42:10 +0300

    allReducers

--- src/index.js
+++ src/index.js
@@ -3,6 +3,50 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+import allReducers from './reducers';
+
+const store = createStore(allReducers);
+
+
+// Store = Global State
+
+// Action = Describes (but not does) what is to be done
+// const increment = () => {
+//   return {
+//     type: 'INCREMENT'
+//   }
+// }
+
+// const decrement = () => {
+//   return {
+//     type: 'DECREMENT'
+//   }
+// }
+
+// Reducer = Changes the state based on a specific action
+// const counter = (state = 0, action) => {
+//   switch (action.type) {
+//     case 'INCREMENT':
+//       return state + 1;
+//     case 'DECREMENT':
+//       return state - 1;
+//     default:
+//       return state;
+//   }
+// };
+
+// Creating a store where counter is a parameter
+// let store = createStore(counter);
+// Displaying store in the console
+// store.subscribe(() => console.log(store.getState()));
+
+
+// Dispatch = Implements an action to the reducer/Executes an action
+// store.dispatch(increment());
+// store.dispatch(increment());
+// store.dispatch(decrement());
+
 
 ReactDOM.render(
   <React.StrictMode>
